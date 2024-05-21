331fde3586ef7f1b879b86f622b1634a6420900b
charokoukuu <charokoukuu@gmail.com>
2024-05-22 02:19:28 +0900

    feat: Update @tsparticles package and remove unused imports

--- components/global/ParticleSystem.tsx
+++ components/global/ParticleSystem.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect, useMemo, useState } from 'react';
 import Particles, { initParticlesEngine } from '@tsparticles/react';
-import { loadSlim } from '@tsparticles/slim'; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+import { loadSlim } from '@tsparticles/slim';
 
 export const ParticleSystem = () => {
   const [init, setInit] = useState(false);
