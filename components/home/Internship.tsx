8278ecc372add2e5ac98e2b0813e2e18956d8ab7
charokoukuu <charokoukuu@gmail.com>
2024-05-21 23:32:26 +0900

    feat: Update Internship component to use data from store

--- components/home/Internship.tsx
+++ components/home/Internship.tsx
@@ -4,6 +4,7 @@ import { Stack, useBreakpointValue } from '@chakra-ui/react';
 import { H1 } from '../global/H1';
 import { Dancing_Script } from 'next/font/google';
 import { InternCard } from './InternCard';
+import { internship } from '@/lib/store/internship';
 const dancing_script = Dancing_Script({ subsets: ['latin'] });
 export const Internship = () => {
   return (
@@ -15,22 +16,11 @@ export const Internship = () => {
         justifyContent={'center'}
         alignItems={'center'}
       >
-        <InternCard
-          name={'ピクシブ株式会社'}
-          term={'2022年9月〜2022年11月(3ヶ月)'}
-        >
-          サービス「pixiv」のフロントエンドUX改善。PC版にはReact+TypeScriptを使用し、SP版にはVue2を使用。改修前のページではPHP(smarty)を使用し、コンポーネントの描画負荷の改善やマンガビューアの新機能実装などを行いました。また、RenderStoneを使った、ユーザエージェントに応じてSSRを行うDynamicRenderingのフロント対応などを行いました。
-        </InternCard>
-        <InternCard
-          name={'チームラボ株式会社'}
-          term={'2023年9月初旬〜2023年9月中旬(2週間)'}
-        >
-          案件であるECサイトの新機能実装。技術スタックはVue2/Vuex3/GoLand(Gin)を使用。描画負荷軽減に配慮したフロントの実装を行いました。また、バックエンドではGinを使用し、クリーンアーキテクチャに則ったAPIエンドポイントの実装を行いました。
-        </InternCard>
-        <InternCard name={'Sky株式会社'} term={'2024年3月〜現在(長期)'}>
-          社内案件管理ツールの新規開発。フロントエンドにはNext.jsのApp
-          Routerを使用し、BFFおよびバックエンドにはNest.js/GraphQL/gRPC/PostgreSQLを使用。データベーススキーマの定義からAPIエンドポイントの実装、フロントエンドの実装までを担当しています。
-        </InternCard>
+        {internship.map((item) => (
+          <InternCard key={item.name} name={item.name} term={item.term}>
+            {item.content}
+          </InternCard>
+        ))}
       </Stack>
     </>
   );
