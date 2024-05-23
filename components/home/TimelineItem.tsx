42d2999a112c1c5e6c5e4dbf4c8a9cfe5f06fe55
charokoukuu <charokoukuu@gmail.com>
2024-05-24 00:36:45 +0900

    chore: Update TimelineItem component to open link without target attribute

--- components/home/TimelineItem.tsx
+++ components/home/TimelineItem.tsx
@@ -21,7 +21,7 @@ export const TimelineItem: React.FC<TimelineItemProps> = ({ data }) => (
     className="timeline-item"
     onClick={() => {
       if (data.link) {
-        window.open(data.link.url);
+        window.open(data.link.url, '_self');
       }
     }}
   >
