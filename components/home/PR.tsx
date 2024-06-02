8bf6b70d5d3b345c109ca5fd5f5669100f7c2e31
charokoukuu <charokoukuu@gmail.com>
2024-06-02 23:40:15 +0900

    chore: Update PR component with additional backend experience

--- components/home/PR.tsx
+++ components/home/PR.tsx
@@ -9,7 +9,7 @@ export const PR = () => {
         <Text className="text-1xl mt-3 text-white">ABOUT ME</Text>
         <Divider borderColor={'#fff'} mt={2} className="mb-3" />
         <Text className="text-white">
-          フロントエンドが得意な大学院生。PdMの経験あり。React、Next.js、Vue.jsに精通。バックエンドではExpress、Nest.jsを用いたNode.jsフレームワークの経験、MongoDB、PostgreSQLを扱う。インターンではgRPCやGraphQLを活用し、マイクロサービスやクリーンアーキテクチャでの開発。フリーランスでWEBサービスの受託開発を行い、ヒアリングから納品までの全工程を担当。ハッカソンでの受賞歴多数。問題解決やチームワークに自信あり。
+          フロントエンドが得意な大学院生。PdMの経験あり。React、Next.js、Vue.jsに精通。バックエンドではExpress、Nest.jsを用いたNode.jsフレームワークやMongoDB、PostgreSQLを扱う。インターンではgRPCやGraphQLを活用し、マイクロサービスやクリーンアーキテクチャでの開発。フリーランスでWEBサービスの受託開発を行い、ヒアリングから納品までの全工程を担当。ハッカソンでの受賞歴多数。問題解決やチームワークに自信あり。
         </Text>
       </Box>
     </div>
